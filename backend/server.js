8a2f85305ba08a033029ee81c6e81d0c55727c25
Kevin Hernandez <Khern165@fiu.edu>
2020-12-04 20:05:03 -0500

    Added backend files containing routers, models, and server/utilities.

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/geektext', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+
+app.use('/api/uploads', uploadRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
